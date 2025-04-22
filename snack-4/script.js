576689f89871e8c8bc1847a8fe1cb4b5da14fa9e
andrea-mosca <andrea.moscateli@gmail.com>
2025-04-22 14:28:34 +0200

    snack 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,6 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+const authorNames = posts.map((name)=>name.author);
+console.log(posts);
+console.log(authorNames);
