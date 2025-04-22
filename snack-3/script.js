89fcf2d3d19a2ca12e5195d448ae6cca262d58ad
andrea-mosca <andrea.moscateli@gmail.com>
2025-04-22 14:23:35 +0200

    snack 3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,9 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+const incrementedNumbers = numbers.map((number)=>{
+    number += 1;
+    return number;
+});
+console.log(numbers);
+console.log(incrementedNumbers);
