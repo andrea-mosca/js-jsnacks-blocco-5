e4e157f7b92ee9bd97d6f16eb722be6de0c453e8
andrea-mosca <andrea.moscateli@gmail.com>
2025-04-22 14:38:52 +0200

    snack 5

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,9 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+const evenNums = [];
+nums.forEach((number) => {
+  if (number % 2 === 0) evenNums.push(number);
+});
+console.log(nums);
+console.log(evenNums);
